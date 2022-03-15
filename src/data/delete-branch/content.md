fa78b599129f3c2c04b396d7b006b3e7c6e0f043
murgatt <12082276+murgatt@users.noreply.github.com>
2022-03-15 17:30:16 +0100

    feat(data): add delete branch

---
+++ src/data/delete-branch/content.md
@@ -0,0 +1,27 @@
+---
+id: delete-branch
+title: Delete branch
+tags:
+    - 'delete'
+    - 'branch'
+---
+
+# Delete branch
+
+## Delete a local branch
+
+```shell
+git branch -d <branch_name>
+```
+
+## Force delete a local branch
+
+```shell
+git branch -D <branch_name>
+```
+
+## Delete a remote branch
+
+```shell
+git push --delete <remote_name> <branch_name>
+```
--- src/data/index.js
+++ src/data/index.js
@@ -2,5 +2,6 @@ import changeCommitMessage from './change-commit-message';
 import createNewBranch from './create-new-branch';
 import changeRemoteTrackingBranch from './change-remote-tracking-branch';
 import switchBranch from './switch-branch';
+import deleteBranch from './delete-branch';
 
-export default [createNewBranch, changeRemoteTrackingBranch, changeCommitMessage, switchBranch];
+export default [createNewBranch, changeRemoteTrackingBranch, changeCommitMessage, switchBranch, deleteBranch];
