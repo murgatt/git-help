fc25457a17f6d14cfc42858ae9ce1f110ede1d8e
Théo <12082276+murgatt@users.noreply.github.com>
2022-07-29 15:03:56 +0200

    feat(content): add revert file or directory (#3)

--- src/data/index.js
+++ src/data/index.js
@@ -1,7 +1,15 @@
 import changeCommitMessage from './change-commit-message';
-import createNewBranch from './create-new-branch';
 import changeRemoteTrackingBranch from './change-remote-tracking-branch';
-import switchBranch from './switch-branch';
+import createNewBranch from './create-new-branch';
 import deleteBranch from './delete-branch';
+import revertFileOrDirectory from './revert-file-or-directory';
+import switchBranch from './switch-branch';
 
-export default [createNewBranch, changeRemoteTrackingBranch, changeCommitMessage, switchBranch, deleteBranch];
+export default [
+    changeCommitMessage,
+    changeRemoteTrackingBranch,
+    createNewBranch,
+    deleteBranch,
+    revertFileOrDirectory,
+    switchBranch,
+];
---
+++ src/data/revert-file-or-directory/content.md
@@ -0,0 +1,35 @@
+---
+id: revert-file-or-directory
+title: Revert a single file or a directory
+tags:
+    - 'revert'
+    - 'rollback'
+    - 'discard'
+    - 'checkout --'
+---
+
+# Revert a single file or a directory
+
+## Revert to the last version
+
+```shell
+git checkout -- <path/to/file/or/directory>
+```
+
+## Revert to a branch
+
+```shell
+git checkout <remote>/<branch> -- <path/to/file/or/directory>
+```
+
+or
+
+```shell
+git checkout <branch> -- <path/to/file/or/directory>
+```
+
+## Revert to a specific commit
+
+```shell
+git checkout <commit_hash> -- <path/to/file/or/directory>
+```
